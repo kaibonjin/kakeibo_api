35146754adbf644061c7c91235c07aa1873aa779
kaibonjin <kaibonjin@gmail.com>
2022-06-11 04:25:47 +0900

    add more columns to expenses table

--- src/main/java/com/api/kakeibo_api/domain/Expense.java
+++ src/main/java/com/api/kakeibo_api/domain/Expense.java
@@ -3,6 +3,8 @@ package com.api.kakeibo_api.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -15,7 +17,21 @@ public class Expense {
 
     @ManyToOne
     private User user;
+
+    @Column(nullable = false)
     private int amount;
+
     private String memo;
+
+    @Column(nullable = false)
     private Date date;
+
+    // TODO add categoryId
+    @CreatedDate
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
 }
