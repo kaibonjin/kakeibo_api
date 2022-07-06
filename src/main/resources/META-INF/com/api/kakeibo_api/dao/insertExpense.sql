5d90e1822b8feef49a9f4c70a749dafea33d5053
kaibonjin <kaibonjin@gmail.com>
2022-07-07 02:00:55 +0900

    add insertExpense Dao sql file

---
+++ src/main/resources/META-INF/com/api/kakeibo_api/dao/insertExpense.sql
@@ -0,0 +1,2 @@
+insert into expenses (id, user_id, amount, memo, date)
+values (/*expenses.id*/0, /*expenses.user_id*/2, /* expenses.amount */100, /* expenses.memo */"支出", /* expenses.date */'2019-09-07')
