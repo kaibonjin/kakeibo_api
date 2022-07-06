4cfb1d9e56adad10642f62eded1177d14bc0b70b
kaibonjin <kaibonjin@gmail.com>
2022-07-07 07:58:19 +0900

    fix pom.xml

--- src/main/resources/META-INF/com/api/kakeibo_api/dao/insertExpense.sql
+++
@@ -1,2 +0,0 @@
-insert into expenses (id, user_id, amount, memo, date)
-values (/*expenses.id*/0, /*expenses.user_id*/2, /* expenses.amount */100, /* expenses.memo */"支出", /* expenses.date */'2019-09-07')
